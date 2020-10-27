631812919996d918dbed4459fc80e3c7df0073f6
杨武 <18501603354@163.com>
2020-10-27 21:29:04 +0800

    367. 有效的完全平方数

---
+++ leetcode_yangwu/valid-perfect-square.py
@@ -0,0 +1,14 @@
+#367. 有效的完全平方数
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False

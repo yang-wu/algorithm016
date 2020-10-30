6eca2159f2837df0b6b3069609e6e8bc04b91fdb
杨武 <18501603354@163.com>
2020-10-30 23:56:58 +0800

    24. 两两交换链表中的节点

---
+++ leetcode_yangwu/swap-nodes-in-pairs.py
@@ -0,0 +1,14 @@
+#24. 两两交换链表中的节点
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newhead = head.next
+        head.next = self.swapPairs(newhead.next)
+        newhead.next = head
+        return newhead

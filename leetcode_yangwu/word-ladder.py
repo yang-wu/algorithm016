275c17f2b0cbde67ea783a72774ebff46da7ecbc
杨武 <18501603354@163.com>
2020-11-11 20:16:31 +0800

    127. 单词接龙

--- leetcode_yangwu/word-ladder.py
+++ leetcode_yangwu/word-ladder.py
@@ -4,22 +4,21 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if not beginWord or not endWord or not wordList or endWord not in wordList:
             return 0
-        n = len(beginWord)
-        hashword = defaultdict(list)
+        m = len(beginWord)
+        hashWord = defaultdict(list)
         for word in wordList:
-            for i in range(n):
-                hashword[word[:i] + "*" + word[i+1:]].append(word)
-        queue = [(beginWord, 1)]
+            for i in range(m):
+                hashWord[word[0:i] + "*" + word[i+1:m]].append(word)
+        queue = [(beginWord, 1)]  #队列保存访问单词和当前长度的元组
         visited = {beginWord: True}
         while queue:
-            curword, level = queue.pop(0)
-            for i in range(n):
-                matchword = curword[:i] + "*" + curword[i+1:]
-                for word in hashword[matchword]:
+            curWord, level = queue.pop(0)
+            for i in range(m):
+                for word in hashWord[curWord[0:i] + "*" + curWord[i+1:m]]: #遍历模糊匹配单词列表是否是结尾单词
                     if word == endWord:
                         return level + 1
-                    if word not in visited.keys():
-                        visited[word] = True
+                    if word not in visited:
                         queue.append((word, level + 1))
-                #hashword[matchword] = []
+                        visited[word] = True
         return 0
+

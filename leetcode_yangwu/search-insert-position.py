301a0befeb5e3a14dd7c5f092ddf843465518546
杨武 <18501603354@163.com>
2020-11-21 00:19:22 +0800

    35. 搜索插入位置

---
+++ leetcode_yangwu/search-insert-position.py
@@ -0,0 +1,15 @@
+#35. 搜索插入位置
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left

373681b5374d7c34e8a72c6e6ad465a123c6b122
杨武 <18501603354@163.com>
2020-12-18 22:52:39 +0800

    110. 平衡二叉树

---
+++ leetcode_yangwu/balanced-binary-tree.py
@@ -0,0 +1,17 @@
+#110. 平衡二叉树
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root):
+            if not root:
+                return 0
+            return max(height(root.left), height(root.right)) + 1
+        
+        if not root:
+            return True
+        return abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)

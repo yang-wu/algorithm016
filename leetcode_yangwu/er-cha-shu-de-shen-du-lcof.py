e3e9fad2700ccef764162ec447097d4be8644e53
杨武 <18501603354@163.com>
2020-12-22 22:15:12 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ leetcode_yangwu/er-cha-shu-de-shen-du-lcof.py
@@ -0,0 +1,13 @@
+#剑指 Offer 55 - I. 二叉树的深度
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1

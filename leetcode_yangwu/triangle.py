cb3cb05c64c058f32a914b6f6b6c766b65a89747
杨武 <18501603354@163.com>
2021-01-17 20:49:19 +0800

    120. 三角形最小路径和

--- leetcode_yangwu/triangle.py
+++ leetcode_yangwu/triangle.py
@@ -9,9 +9,10 @@ class Solution:
         for i in range(1, m):
             for j in range(0, i+1):
                 if j == 0:
-                    dp[i][j] = dp[i-1][j] + triangle[i][j]
-                elif j == i:
-                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                    dp[i][j] = dp[i-1][j] + triangle[i][j]  #三角形最左边
+                elif j == i: 
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]  #三角形最右边
                 else:
                     dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
         return min(dp[m-1])
+

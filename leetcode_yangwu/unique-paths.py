27194d2fdc6d7d2491f8d692ad890c16a9d1a5f4
杨武 <18501603354@163.com>
2021-01-17 21:27:18 +0800

    62. 不同路径

--- leetcode_yangwu/unique-paths.py
+++ leetcode_yangwu/unique-paths.py
@@ -1,13 +1,9 @@
 #62. 不同路径
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for i in range(m)]
-        for i in range(m):
-            dp[i][0] = 1
-        for j in range(n):
-            dp[0][j] = 1
-        for i in range(1, m):
-            for j in range(1, n):
+        dp = [[1] * n for i in range(m)]
+        for i in range(1, m):        # 第0行默认都是1种走法
+            for j in range(1, n):    #第0列默认都是1种走法
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
 
